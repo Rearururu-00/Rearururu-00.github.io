84619fea3bd76c27032a1fe778cf26598069106e
Rearururu-00 <92760959+Rearururu-00@users.noreply.github.com>
2021-10-22 11:28:31 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function fname(){
-  var n = document.getElementById('name').value;
-  window.alert(n)
+var a;
+
+function getName(){
+  //get value from input field and save as a
+  a = document.getElementById("username").value;
+  //create LS key with value of a
+  localStorage.setItem("uName",a);
+  //take user to two.html
+  location.href - "two.html"
+}
+
+function getUser(){
+  var b = localStorage.getItem("uName");
+  document.getElementById("placeholder").innerHTML - "Hello " +b;
 }
