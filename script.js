cfaa5ce4031a692c3ac277226de02adeea9ff01b
Rearururu-00 <92760959+Rearururu-00@users.noreply.github.com>
2021-10-25 12:04:33 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-var a;
+var fieldValue;
 
-function getName(){
+function saveName(){
   //get value from input field and save as a
-  a = document.getElementById("username").value;
+  fieldValue = document.getElementById("textfield").value;
   //create LS key with value of a
-  localStorage.setItem("uName",a);
+  localStorage.setItem("userName",fieldValue);
   //take user to two.html
   location.href = "two.html"
-}
-
-function getUser(){
-  var b = localStorage.getItem("uName");
-  document.getElementById("placeholder").innerHTML = "Hello " +b;
 }
