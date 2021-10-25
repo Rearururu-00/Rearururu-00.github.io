b7267a955b430b6f88938b66c2c0c58240f52462
Rearururu-00 <92760959+Rearururu-00@users.noreply.github.com>
2021-10-25 12:06:06 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-var fieldValue;
-
 function saveName(){
   //get value from input field and save as a
-  fieldValue = document.getElementById("textfield").value;
+  var fieldValue = document.getElementById("textfield").value;
   //create LS key with value of a
   localStorage.setItem("userName",fieldValue);
   //take user to two.html
